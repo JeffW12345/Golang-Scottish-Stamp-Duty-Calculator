75dbb9cb86e357ce2d1c61b735bcdc8f6346c23f
Jeff Waters <watersjg@gmail.com>
2024-06-06 17:48:06 +0100

    added unit test for timeout check functionality

--- tax_band_calculator_entry_point_test.go
+++ tax_band_calculator_entry_point_test.go
@@ -6,10 +6,31 @@ import (
 	"bou.ke/monkey"
 )
 
+func didPanicHappen() bool {	
+	return recover() != nil
+}
+
+func mockUpdateChannelWhenServerReady(isServerReady chan bool) {
+	
+	go updateChannelWhenServerReady(isServerReady)
+
+}
+
 func UpdateChannelWhenServerReadyTest(t *testing.T) {
 	defer monkey.UnpatchAll()
 
 	t.Run("Should update channel if no error returned from API", func(t *testing.T) {
+		mockIsServerReady := make(chan bool)
+		monkey.Patch(getTaxDueForPropertyOfValue, func() (float32, error) {return 0, nil})
+		updateChannelWhenServerReady(mockIsServerReady)
+		channelContent, channelUpdated := <-mockIsServerReady
+
+		if !channelUpdated || !channelContent {
+			t.Error("channel not updated when getTaxDueForPropertyOfValue returns an error")
+		}
+	})
+
+	t.Run("Should panic if server not ready after 1 second", func(t *testing.T) {
 		mockIsServerReady := make(chan bool)
 		monkey.Patch(getTaxDueForPropertyOfValue, func() (float32, error) {return 0, nil})
 		updateChannelWhenServerReady(mockIsServerReady)
