839a0b2c9cbb8e51bea80164f218051062c40b4f
Jeff Waters <watersjg@gmail.com>
2024-06-06 17:18:47 +0100

    minor update to test file

--- go.mod
+++ go.mod
@@ -3,5 +3,3 @@ module tax-calculator
 go 1.22.3
 
 require bou.ke/monkey v1.0.2
-
-require github.com/stretchr/testify v1.9.0 // indirect
--- tax_band_calculator_entry_point_test.go
+++ tax_band_calculator_entry_point_test.go
@@ -19,15 +19,4 @@ func UpdateChannelWhenServerReadyTest(t *testing.T) {
 			t.Error("channel not updated when getTaxDueForPropertyOfValue returns an error")
 		}
 	})
-
-	t.Run("Should update channel if no error returned from API", func(t *testing.T) {
-		mockIsServerReady := make(chan bool)
-		monkey.Patch(getTaxDueForPropertyOfValue, func() (float32, error) {return 0, nil})
-		updateChannelWhenServerReady(mockIsServerReady)
-		channelContent, channelUpdated := <-mockIsServerReady
-
-		if !channelUpdated || !channelContent {
-			t.Error("channel not updated when getTaxDueForPropertyOfValue returns an error")
-		}
-	})
 }
