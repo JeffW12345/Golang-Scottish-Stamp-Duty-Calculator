5c1dfe6d5978e0e13dee3da51740cbd84cdeaace
JeffW12345 <watersjg@gmail.com>
2024-06-14 10:52:12 +0100

    Got API to display error messages

--- resources/form-submission-handling.js
+++ resources/form-submission-handling.js
@@ -9,6 +9,10 @@ document.addEventListener('DOMContentLoaded', function() {
         try {
             const response = await fetch(url);
             if (!response.ok) {
+                const errorMessageObject = await response.json();
+                document.getElementById("results").style.display = "None";
+                document.getElementById("error-handling").style.display = "block";
+                document.getElementById("error-message").textContent = errorMessageObject.message
                 throw new Error('Network response was not ok: ' + response.statusText);
             }
 
@@ -16,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById("valueOfProperty").textContent = String(parseFloat(propertyValue).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             document.getElementById("taxDue").textContent = String(parseFloat(taxDueObject.taxDue).toFixed(2)).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
             document.getElementById("results").style.display = "block";
+            document.getElementById("error-handling").style.display = "None";
         } catch (error) {
             console.error('Fetch error:', error);
         }
